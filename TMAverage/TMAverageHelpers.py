e7ad89cf038f2f2d6e651e9d778e3651bcb4bb44
Robert Schmidt <Robert.Schmidt@lasp.colorado.edu>
2026-01-19 20:52:54 +0000

    Updated version number

--- TMAverage/TMAverageHelpers.py
+++ TMAverage/TMAverageHelpers.py
@@ -27,7 +27,7 @@
 ##########################################################################
 import sys
 
-TMAVERAGE_VERSION = "2.0"
+TMAVERAGE_VERSION = "3.0"
 
 # Dictionary of databases and SIDs that the script is designed for. Any attempt 
 # to run any TMAverage scripts on SIDs or databases not listed here will fail and 
