dd87999a29669d7be719fd83ce60bc6324508bd8
Robert Schmidt <Robert.Schmidt@lasp.colorado.edu>
2026-02-10 02:26:43 +0000

    Added previous versions

---
+++ TMAverage/v3.0/TMAverageHelpers.py
@@ -0,0 +1,270 @@
+# Purpose: This python script stores all of the static variables for TMAverage, and
+#          generates bash in order to allows the relevant bash scripts to access the
+#          configuration information.
+# 
+# 
+# Notes:   The configuration details are database-dependent, and the script needs to be
+#          passed the database name in order to correctly output the appropriate bash.
+# 
+#          For more detailed documentation go to https://confluence.lasp.colorado.edu/spaces/MODSDB/pages/228214621/TMAverage+-+Usage+Performance
+# 
+# IMPORTANT: The ConfigureTMAverageEnvironment.sh script runs checks to make sure the DDL is 
+#            up-to-date by checking the most recent DDL changes, which are indicated by the 
+#            tmaverage_table_check_columns and tmaverage_stats_check_columns variables. These
+#            should be update any time the DDL is updated in order to keep the check up-to-date.
+# 
+# Usage:
+#          python TMAverageHelpers.py [ <database> <system_id | list> ] OR [ version ]
+# 
+# Examples:
+#          python TMAverageHelpers.py ixpeprod 1
+#          python TMAverageHelpers.py version
+#          python TMAverageHelpers.py emadev list
+# 
+# Author: Robert Schmidt
+# Created: August 25th, 2025
+# Last Modified: November 17th, 2025 - RS
+##########################################################################
+import sys
+
+TMAVERAGE_VERSION = "3.0"
+
+# Dictionary of databases and SIDs that the script is designed for. Any attempt 
+# to run any TMAverage scripts on SIDs or databases not listed here will fail and 
+# display an appropriate error message.
+SUPPORTED_DB_SIDS = {
+    "goldprod": [1],
+    "evep12c": [1],
+    "tsisprod": [1],
+    "aimprod": [1],
+    "ixpeprod": [1],
+    "emadev": [19, 20],
+}
+
+# The schema where data is primarily stored (typically L1A), and where TMAverage tables
+# are located.
+DATA_SCHEMAS = {
+    "aimprod": "AIM_L1A",
+    "goldprod": "GOLD_L1A",
+    "evep12c": "EVE_L1A",
+    "tsisprod": "TSIS_L1A",
+    "ixpeprod": "IXPE_L1A",
+    "emadev": "EMA_SCHEMA<SID>"  # (EMA) Data is separated by SID/Schema
+}
+
+# Primary metadata schemas.
+CT_SCHEMAS = {
+    "aimprod": "AIM_CT_SC",
+    "goldprod": "GOLD_CT",
+    "evep12c": "EVE_CT",
+    "tsisprod": "TSIS_CT",
+    "ixpeprod": "IXPE_CT",
+    "emadev": "EMA_CT",
+}
+
+# The location and name of TMAnalog. Depends on SID.
+TMANALOG_DBS = {
+    "evep12c":  "<DATA_SCHEMA>.TMANALOG",
+    "emadev":   "<DATA_SCHEMA>.TMANALOG",
+    "aimprod":  "<DATA_SCHEMA>.TMANALOG_TABLE",
+    "*":        "<DATA_SCHEMA>.TMANALOG_SID<SID>",
+}
+
+# The non-schema prefixed name of the TMAverage table. Separated out in order to derive
+# the primary key name from it effectively without needing to truncate the schema name from it.
+TMAVERAGE_TAB_NAME = {
+    "emadev":   "TMAVERAGE",
+    "*":        "TMAVERAGE_SID<SID>"
+}
+
+# Full TMAverage table name.
+TMAVERAGE_TABLE_NAME = "<DATA_SCHEMA>.<TMAVERAGE_TAB_NAME>"
+
+# Full TMAverage stats name.
+TMAVERAGE_STATS_NAME = "<DATA_SCHEMA>.TMAVERAGE_STATS"
+
+# Name of the tablespace where TMAverage and TMAverage_Stats are stored.
+TABLESPACE_NAME = {
+    "emadev":   "<DATA_SCHEMA>_TMAVERAGE",
+    "*":        "TMAVERAGE_SID<SID>",
+}
+
+# TelemetryItemDefinition stores a list of all TMIDs, as well as their datatype.
+TELEMETRYITEMDEFINITION_DBS = {
+    "*":  "<CT_SCHEMA>.TELEMETRYITEMDEFINITION",
+}
+
+# Defines polynomial conversions of the raw data into EU (Engineering Units). 
+TELEMETRYANALOGCONVERSIONS_DBS = {
+    "emadev":   "<DATA_SCHEMA>.TMANALOGCONVERSIONS",   # (EMA) Separate analog conversions for each SID.
+    "*":        "<CT_SCHEMA>.TELEMETRYANALOGCONVERSIONS",
+}
+
+# Contains the column to use for time-variant analog conversions. If conversions are not time-variant, 
+# contain an empty string.
+TIME_VARIANT_ANALOG_CONVERSION = {
+    "emadev":   "true",
+    "*":        ""
+}
+
+# What time column to use based on database. EMA is using ASCT (Adjusted time) rather than SCT or ERT.
+# This is separated from OTFD_TIME_COLUMN because the table column name is SCT_VTCW rather than SCT.
+TABLE_TIME_COLUMN = {
+    "emadev": "ASCT",
+    "*": "SCT_VTCW"
+}
+
+OTFD_TIME_COLUMN = {
+    "emadev": "ASCT",
+    "*": "SCT"
+}
+
+# The DDL to create the TMAverage table. This requires TMAVERAGE_TAB_NAME to derive
+# the primary key constraint name so that duplicates indexes are not created during testing
+# When this is updated, update the tmaverage_table_check_columns variable as well.
+# Note: This is ONLY executed in ConfigureTMAverageEnvironment.sh, and the escape characters (\\) 
+#       present to allow for the bash variable assignment to succeed without interpreting it literally.
+TMAVERAGE_TABLE_DDL=f"""CREATE TABLE <TMAVERAGE_TABLE_NAME>
+(
+    TMID NUMBER(7,0) NOT NULL ENABLE,
+    <TABLE_TIME_COLUMN> NUMBER(16,0) NOT NULL ENABLE,
+    AVERAGE_VALUE FLOAT(126) NOT NULL ENABLE,
+    MINIMUM_VALUE FLOAT(126) NOT NULL ENABLE,
+    MAXIMUM_VALUE FLOAT(126) NOT NULL ENABLE,
+    VALUE_COUNT NUMBER(7,0) NOT NULL ENABLE,
+    CONSTRAINT PK_<TMAVERAGE_TAB_NAME> PRIMARY KEY (TMID, <TABLE_TIME_COLUMN>) ENABLE
+)
+ORGANIZATION INDEX PCTFREE 0 LOGGING
+TABLESPACE \\"<TABLESPACE_NAME>\\";"""
+# Should always check that the loaded DDL has the correct time column set.
+TMAVERAGE_TABLE_CHECK_COLUMNS="<TABLE_TIME_COLUMN>"
+
+# When this is updated, update the tmaverage_stats_check_columns variable as well.
+TMAVERAGE_STATS_DDL=f"""CREATE TABLE <TMAVERAGE_STATS_NAME> (
+    DATABASE_NAME   VARCHAR2(128),
+    START_TIME      TIMESTAMP PRIMARY KEY,
+    RUN_PARAMS          VARCHAR(200),
+    TIME_RAN        INTERVAL DAY TO SECOND,
+    FAILED          NUMBER(1),
+    CANCELLED       NUMBER(1),
+    ROWS_READ       NUMBER,
+    ROWS_RETURNED   NUMBER,
+    UNIQUE_CONSTRAINT_NUM NUMBER,
+    OTFD_ERROR_NUM  NUMBER,
+    ERRORS          CLOB
+)
+TABLESPACE \\"<TABLESPACE_NAME>\\";"""
+TMAVERAGE_STATS_CHECK_COLUMNS="ROWS_READ ROWS_RETURNED RUN_PARAMS"
+
+class OTFDException(Exception):
+    """
+    Custom exception class for OTFD errors. Includes an array of rows from ONTHEFLYDECOM_ERRORS.
+    """
+
+class TMAverageConfigs():
+    """
+    Class that parses and allows access to TMAverage config parameters. Each instance variable in this class
+    is used to substitute out the markers demarcated by <VARIABLE_NAME>, allowing for updates to a single 
+    parameter to cascade and update all dependent parts. For example, if a config contains the string 
+    "TEST_SCHEMA<SID>", then if the SID variable exists when that config is parsed, then the <SID> marker
+    gets replaced by the value of the variable.
+
+    Note: This is necessary both so that there is a standard way to set the configs, but also so that
+          this data can be passed to the worker threads in a convenient manner. Previously they just 
+          accessed the constants in TMAverageHelpers.py, but now the configuration needs to be evaluated at 
+          runtime to handle multiple possible SIDs, and this allows for a single variable to be passed to 
+          the worker threads.
+    """
+    def __init__(self, database: str, sid: str):
+        # Check that database and sid are supported
+        if not (database in SUPPORTED_DB_SIDS.keys() and int(sid) in SUPPORTED_DB_SIDS[database]):
+            raise ValueError(f"ERROR: Database {database} SID {sid} is not supported by TMAverage.\n"
+                             "Supported databases are: {str(tuple(SUPPORTED_DB_SIDS.keys()))}")
+
+        self.VERSION = TMAVERAGE_VERSION
+
+        self.SID = sid
+        self.DATABASE = database
+
+        self.DATA_SCHEMA = self.parse_config(DATA_SCHEMAS, database)
+        self.CT_SCHEMA = self.parse_config(CT_SCHEMAS, database)
+
+        self.TMAVERAGE_TAB_NAME = self.parse_config(TMAVERAGE_TAB_NAME, database)
+        self.TMANALOG_TABLE_NAME = self.parse_config(TMANALOG_DBS, database)
+        self.TMAVERAGE_TABLE_NAME = self.parse_config(TMAVERAGE_TABLE_NAME, database)
+        self.TMAVERAGE_STATS_NAME = self.parse_config(TMAVERAGE_STATS_NAME, database)
+
+        self.TABLESPACE_NAME = self.parse_config(TABLESPACE_NAME, database)
+
+        self.TELEMETRY_ANALOG_CONVERSIONS_NAME = self.parse_config(TELEMETRYANALOGCONVERSIONS_DBS, database)
+        self.TELEMETRY_ITEM_DEFINITIONS_NAME = self.parse_config(TELEMETRYITEMDEFINITION_DBS, database)
+        self.TIME_VARIANT_ANALOG_CONVERSION = self.parse_config(TIME_VARIANT_ANALOG_CONVERSION, database)
+
+        self.TABLE_TIME_COLUMN = self.parse_config(TABLE_TIME_COLUMN, database)
+        self.OTFD_TIME_COLUMN = self.parse_config(OTFD_TIME_COLUMN, database)
+        
+        self.TMAVERAGE_TABLE_DDL = self.parse_config(TMAVERAGE_TABLE_DDL, None)
+        self.TMAVERAGE_TABLE_CHECK_COLUMNS = self.parse_config(TMAVERAGE_TABLE_CHECK_COLUMNS, None)
+
+        self.TMAVERAGE_STATS_DDL = self.parse_config(TMAVERAGE_STATS_DDL, None)
+        self.TMAVERAGE_STATS_CHECK_COLUMNS = self.parse_config(TMAVERAGE_STATS_CHECK_COLUMNS, None)
+
+    def parse_config(self, static_config, config_key: str):
+        """
+        Uses the instance variables already defined to replace placeholders in a newly added config item dynamically.
+        """
+        if isinstance(static_config, dict):
+            try:
+                config_item = static_config[config_key]
+            except KeyError:
+                try:
+                    config_item = static_config["*"] # Try to get default value instead.
+                except:
+                    raise ValueError(f"ERROR: Config key {config_key} not supported. Database/SID passed is not valid.")
+        elif isinstance(static_config, str):
+            config_item = static_config
+        else:
+            raise ValueError("ERROR: Config must be of type dict or string")
+        
+        # Gets the configs that were populated so far.
+        attributes = vars(self)
+
+        for key in attributes.keys():
+            config_item = config_item.replace(f"<{key}>", str(attributes[key]))
+        
+        if "<" in config_item and ">" in config_item:
+            print("WARNING: Unreplaced substitution present in config: ", file=sys.stderr)
+            print(config_item, file=sys.stderr)
+        
+        return config_item
+
+# Gets called directly by bash scripts to set the correct environment variables. 
+# Print out the variable assignments, and the bash script will execute them via `exec`.
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] == "version":
+        print(TMAVERAGE_VERSION)
+        exit(0)
+    if len(sys.argv) != 3:
+        print("ERROR: Must take database and SystemID parameter.")
+        exit(1)
+
+    database = sys.argv[1]
+    sid = sys.argv[2]
+
+    if sid == "list":
+        try:
+            print(" ".join([str(x) for x in SUPPORTED_DB_SIDS[database]]))
+            exit(0)
+        except KeyError:
+            print(f"Database {database} is not supported by TMAverage. Exiting...")
+            exit(1)
+    try:
+        int(sid)
+    except ValueError:
+        raise ValueError("SystemID must be an integer")
+
+    conf=TMAverageConfigs(database, sid)
+    attributes = vars(conf)  # Gets dict of variables and their values
+
+    for attribute in attributes:
+        print(f"export {attribute.lower()}=\"{attributes[attribute]}\"")
---
+++ TMAverage/v3.2/DeleteTMAverageData.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+#
+# Purpose:  This script deletes data from TMAverage when given a date input. The script allows a CSV of dates/date ranges 
+#           as follows: 
+#           Single Date: DD-MMM-YY
+#           Date Range: DD-MMM-YY:DD-MMM-YY
+# 
+# Notes:    This script uses TMAverageHelpers.py to set various static parameters that vary by 
+#           database. Any updates to tmaverage configurations must be done in TMAverageHelpers.py
+# 
+#           For more detailed documentation go to https://confluence.lasp.colorado.edu/spaces/MODSDB/pages/228214621/TMAverage+-+Usage+Performance
+# 
+# Author: Robert Schmidt
+# Created on: January 19th, 2026
+# Modified on: January 26th, 2026 - RS
+###############################################################
+usage="Usage: ./DeleteTMAverageData.sh [ -y (Skip confirmation prompt) ] [ -l (optional, list # of rows)] [ database ] [ system_id ] [ dates_to_delete (CSV of dates/date ranges. See docstring) ]"
+example1="Example: ./DeleteTMAverageData.sh -l ixpeprod 1 20-DEC-25,21-DEC-25"
+example2="         ./DeleteTMAverageData.sh -y emadev 20 01-JAN-26:05-JAN-26,07-JAN-26"
+
+list_opt=0
+yes_opt=0
+# Process input options
+while getopts ":hly" option; do
+    case $option in
+    h)
+        echo "$usage"
+        echo "$example1"
+        echo "$example2"
+        exit 0
+        ;;
+    l)
+        list_opt=1
+        ;;
+    y)
+        yes_opt=1
+        ;;
+    \?)
+        echo "ERROR: Invalid option. Exiting..."
+        exit 1
+        ;;
+    esac
+done
+
+shift $(($OPTIND -1))
+
+# Resolve the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+if [ $# -ne 3 ]; then
+    echo "Invalid parameters."
+    echo "$usage"
+    echo "$example1"
+    echo "$example2"
+    exit 1
+fi
+
+if [ $yes_opt -ne 0 ] && [ $list_opt -ne 0 ]; then
+    echo "The -y and -l options are mutually exclusive. Exiting..."
+    exit 1
+fi
+
+export ORACLE_SID=${1,,}
+system_id=${2}
+dates_to_delete=${3^^}
+
+
+sid_check=$("$HOME/common/oracle/VerifyAllParam.sh" -I)
+if [ -n "$sid_check" ]; then
+    if [ "$sid_check" == "-1" ]; then
+        echo "ERROR: \$ORACLE_SID not set. Exiting..."
+        exit 1
+    fi
+    echo "ERROR: Provided database is not open. Exiting..."
+    exit 1
+fi
+
+# If system_id is set, check it.
+if ! [[ $system_id =~ ^[0-9]+$ ]]; then
+    echo "ERROR: System_ID must be a valid integer. Exiting..."
+    exit 1
+fi
+
+# Check for existence of Python virtual environment
+VENV_ACTIVATE="${SCRIPT_DIR}/venv/bin/activate"
+if [ ! -f "$VENV_ACTIVATE" ]; then
+    echo "ERROR: File venv/bin/activate not found in $SCRIPT_DIR! Please run './ConfigureTMAverageEnvironment.sh -v' to create one with the necessary dependencies. Exiting..."
+    exit 1
+fi
+
+# Source activate file for the Python virtual environment
+source "$VENV_ACTIVATE"
+
+# Virtual environment check
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "ERROR: A valid Python virtual environment must exist in $SCRIPT_DIR. Please run './ConfigureTMAverageEnvironment.sh -v' to create one with the necessary dependencies. Exiting..."
+    exit 1
+fi
+
+# Set config variables to default values:
+tmaverage_table_name="";table_time_column="";
+
+# Set TMAverage static values from helper script. If the database name is not supported, this will fail.
+# Set static values from helper script. If the database name is not supported, this will fail.
+var_commands=$(python "$SCRIPT_DIR/TMAverageHelpers.py" "$ORACLE_SID" "$system_id" 2>&1)
+if [ $? -ne 0 ]; then
+    if [[ "$var_commands" == *"not supported by TMAverage"* ]]; then
+        echo "ERROR: Database $ORACLE_SID system_id $system_id is not supported by TMAverage. Exiting..."
+    else
+        echo "$var_commands"
+        echo "ERROR: An error occurred while parsing configs. Exiting..."
+    fi
+    exit 1
+fi
+
+eval "$var_commands"
+
+convert_dt2gps (){
+    datetime_value=${1^^}
+    gps_timestamp=$("$ORACLE_HOME/bin/sqlplus" -s / as sysdba <<EOD | xargs
+        set heading off
+        set feedback off
+        whenever oserror exit 1
+        whenever sqlerror exit 1
+
+        set numwidth 16
+
+        -- The ALTER SESSION is only required for AIM, as all other DBs have the correct
+        -- default set.
+        ALTER SESSION SET NLS_TIMESTAMP_FORMAT='DD-MON-RR HH.MI.SSXFF AM';
+        SELECT DT2GPS('$datetime_value') FROM dual;
+EOD
+    )
+    if [ $? -ne 0 ] || [[ "$gps_timestamp" == *"ORA-"* ]]; then
+        echo "$gps_timestamp"
+        echo "An error occurred while computing GPS timestamp for $datetime_value. Exiting..."
+        exit 1
+    fi
+
+    echo "$gps_timestamp"
+}
+
+# Start with 1=0 so that all additional restrictions can be simply added as 'OR <CONDITION>'
+where_clause="WHERE 1=0"
+
+IFS=','
+for date in $dates_to_delete; do
+    IFS=':' read -r start_date end_date <<< "$date"
+    end_date=${start_date:-$end_date}
+
+    start_time_gps=$(convert_dt2gps "$start_date 12.00.00.000000000 AM")
+    if [ $? -ne 0 ]; then
+        echo "$start_time_gps"
+        exit 1
+    fi
+    # If end_date does not exist, use start_date instead.
+    end_time_gps=$(convert_dt2gps "$end_date 11.59.59.999999999 PM")
+    if [ $? -ne 0 ]; then
+        echo "$end_time_gps"
+        exit 1
+    fi
+
+    where_clause+=" OR $table_time_column BETWEEN $start_time_gps AND $end_time_gps" 
+done
+
+echo "Getting count of # of rows affected."
+
+tmaverage_delete_count=$("$ORACLE_HOME/bin/sqlplus" -s / as sysdba <<EOD | xargs
+    set heading off
+    set feedback off
+    whenever oserror exit 1
+    whenever sqlerror exit 1
+
+    set numwidth 16
+
+    SELECT COUNT(*) FROM $tmaverage_table_name $where_clause;
+EOD
+)
+if [ $? -ne 0 ] || [[ $tmaverage_delete_count == *"ORA-"* ]]; then
+    echo "$tmaverage_delete_count"
+    echo "An error occurred while getting number of records to delete. Exiting..."
+    exit 1
+fi
+
+echo "TMAverage Delete Count: $tmaverage_delete_count"
+
+if [ $list_opt -ne 0 ]; then
+    echo "Finished listing. Exiting..."
+    exit 0
+fi
+if [ $yes_opt -ne 0 ]; then
+    echo "The -y option was provided, so skipping confirmation..."
+else
+    read -r -p "Are you sure you want to delete $tmaverage_delete_count rows? (Y/N): " confirm && confirm=${confirm^^}
+    if ! [ "$confirm" == "Y" ] || [ "$confirm" == "YES" ]; then
+        echo "Cancelling Delete."
+        exit 0
+    fi
+fi
+
+tmaverage_delete_rows=$("$ORACLE_HOME/bin/sqlplus" -s / as sysdba << EOD | xargs
+    set heading off
+    set feedback off
+    whenever oserror exit 1
+    whenever sqlerror exit 1
+
+    DELETE FROM $tmaverage_table_name $where_clause;
+    COMMIT;
+EOD
+)
+if [ $? -ne 0 ] || [[ "$tmaverage_delete_rows" == *"ORA-"* ]]; then
+    echo "$tmaverage_delete_rows"
+    echo "An error occurred while deleting records. Exiting..."
+    exit 1
+fi
+
+echo "Successfully deleted rows from TMAverage."
+exit 0
