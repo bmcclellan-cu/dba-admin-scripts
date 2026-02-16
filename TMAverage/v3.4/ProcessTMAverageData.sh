3df25a9883ae83e85640df1c67630e8aeac6b09e
Robert Schmidt <Robert.Schmidt@lasp.colorado.edu>
2026-02-16 17:11:03 +0000

    Updated LOGDIR

--- TMAverage/v3.4/ProcessTMAverageData.sh
+++ TMAverage/v3.4/ProcessTMAverageData.sh
@@ -299,10 +299,10 @@ fi
 echo "Running... $SCRIPT_DIR/ProcessTMAverageData.py $otfd_opt $exclude_opt $database $tmid $start_date $end_date $parallel_degree"
 
 # Execute the Python script with the prepared arguments
-echo "Running ProcessTMAverageData.py. See log output at /export/home/oracle/tmaverage_logs/"
+echo "Running ProcessTMAverageData.py. See log output at $LOGDIR"
 python "$SCRIPT_DIR/ProcessTMAverageData.py" $otfd_opt $exclude_opt "$database" "$system_id" "$tmid" "$start_date" "$end_date" "${parallel_degree}"
 if [ $? -ne 0 ]; then
-    echo "An error occurred while running ProcessTMAverageData.py. See log outputs at /export/home/oracle/tmaverage_logs/ for more information. Exiting..."
+    echo "An error occurred while running ProcessTMAverageData.py. See log outputs at $LOGDIR for more information. Exiting..."
     exit 1
 fi
 
